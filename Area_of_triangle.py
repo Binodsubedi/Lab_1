9c193e208d62c56408e277e39092e11fa94170db
Binod-Amrit <binodsubedi125@gmail.com>
2021-05-31 10:02:13 +0545

    first commit

---
+++ Area_of_triangle.py
@@ -0,0 +1,5 @@
+Base = int(input("Enter the base length of the triangle:"))
+Height = int(input("Enter the height of the triangle:"))
+Area = 1/2* Base * Height
+
+print("Area of your prefered triangle is ", Area)
